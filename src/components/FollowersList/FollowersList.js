919d0fa4cca3fc10d0b80e7eadd92ece3a766f0f
manthan-l-simformsolutions <manthan.l@simformsolutions.com>
2022-09-28 10:34:48 +0530

    first commit

---
+++ src/components/FollowersList/FollowersList.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import "./FollowersList.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+export default function FollowersList() {
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    fetchFollowers();
+  }, []);
+
+  const fetchFollowers = async () => {
+    const { data } = await axios.get("https://randomuser.me/api/?results=5");
+    setFollowers(data.results);
+  };
+  return (
+    <div className="followerslist-container">
+      {!followers.length ? (
+        <div>Loading...</div>
+      ) : (
+        <div>
+          {followers.map((follower, index) => (
+            <div
+              key={index}
+              className="follower-item"
+              data-testid={`follower-item-${index}`}
+            >
+              <img src={follower.picture.large} />
+              <div className="followers-details">
+                <div className="follower-item-name">
+                  <h4>{follower.name.first}</h4> <h4>{follower.name.last}</h4>
+                </div>
+                <p>{follower.login.username}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+
+      <div className="todo-footer">
+        <Link to="/">Go Back</Link>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -0,0 +1,45 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import FollowersList from "../FollowersList";
+
+const MockFollowersList = () => {
+  return (
+    <BrowserRouter>
+      <FollowersList />
+    </BrowserRouter>
+  );
+};
+
+describe("FollowersList", () => {
+  beforeEach(() => {
+    console.log("RUNS BEFORE EACH TEST");
+    jest.mock("../../../__mocks__/axios");
+  });
+
+  beforeAll(() => {
+    console.log("RUNS ONCE BEFORE ALL TESTS");
+  });
+
+  afterEach(() => {
+    console.log("RUNS AFTER EACH TEST");
+  });
+
+  afterAll(() => {
+    console.log("RUNS ONCE AFTER ALL TESTS");
+  });
+
+  it("should fetch and render input element", async () => {
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findByTestId(`follower-item-0`);
+    expect(followerDivElement).toBeInTheDocument();
+    const followerDivElements = await screen.findAllByRole(`heading`, {
+      name: "Laith",
+    });
+    expect(followerDivElements).toHaveLength(1);
+  });
+  it("Display loading while data feching", async () => {
+    render(<MockFollowersList />);
+    const followerDivElement = await screen.findByText(`Loading...`);
+    expect(followerDivElement).toBeInTheDocument();
+  });
+});
